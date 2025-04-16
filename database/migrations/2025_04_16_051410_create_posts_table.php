5e9d4b1da3d5e494ed0a31ccb92fdb1e33de508c
limjunhau1403 <limjunhau1403@1utar.my>
2025-04-16 13:26:03 +0800

    Remove migration and create simple migration file

--- database/migrations/2025_04_16_051410_create_posts_table.php
+++ database/migrations/2025_04_16_051410_create_posts_table.php
@@ -4,18 +4,30 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration {
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id(); // Primary Key
-            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Foreign Key (User)
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title');
             $table->text('content');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down()
     {
         Schema::dropIfExists('posts');
